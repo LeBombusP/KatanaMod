62fef03fb8abcd86b587966b06f0a25de3be8b48
LeBombusP <dfszd937@gmail.com>
2022-08-25 15:45:40 +0200

    Working Prototype

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,14 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +18,17 @@ public class ExampleMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+//	public static final Katana KATANA = new Katana(new FabricItemSettings().group(ItemGroup.COMBAT).rarity(Rarity.RARE));
+
+	public static ToolItem KATANA = new SwordItem(KatanaMaterial.INSTANCE, 5, -2F, new Item.Settings().group(ItemGroup.COMBAT).rarity(Rarity.RARE));
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier("katanas","katana"), KATANA);
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/net/fabricmc/example/KatanaMaterial.java
@@ -0,0 +1,40 @@
+package net.fabricmc.example;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class KatanaMaterial implements ToolMaterial {
+
+    public static final KatanaMaterial INSTANCE = new KatanaMaterial();
+
+    @Override
+    public int getDurability() {
+        return 460;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 0F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 0F;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 15;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.IRON_INGOT);
+    }
+}
